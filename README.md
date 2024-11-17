051fecd534d0a64d4c4d2b96cced96d7001c36a3
Sebastian <serunmal@gmail.com>
2024-11-17 11:41:49 +0100

    Call serialize and deserialize from main

--- README.md
+++ README.md
@@ -3,11 +3,15 @@ https://codingchallenges.fyi/challenges/challenge-huffman/
 
 Run with
 ```shell
-swift run huffer --file ..\135-0.txt
+swift run huffer serialize --read-file ..\sample.txt --write-file ../testfile.bin
+swift run huffer deserialize --read-file ..\testfile.bin --write-file ../new.out
 ```
 
-# Remarks
-- There seems to be a problem if everything is in a huffer folder
+## Performance
+Reading 1000 chars takes about 0.5 to 1 second.
+
+## Remarks
+- There is a problem if everything is in a huffer folder
+
 # todo
-- can argument parser be called with sub parameters read/write
 - check https://stackoverflow.com/questions/56533917/how-do-i-write-huffman-encoded-characters-to-a-file
--- Sources/Huffer/Huffer.swift
+++ Sources/Huffer/Huffer.swift
@@ -3,44 +3,102 @@ import Foundation
 import HufferLib
 
 @main
-struct Huffer: ParsableCommand {
-    @Option(help: "Specify the file")
-    public var file: String
+struct Huffer: AsyncParsableCommand {
+    static let configuration = CommandConfiguration(
+        subcommands: [Serialize.self, Deserialize.self,])
+}
 
-    mutating func run() throws {
-        print(self.file)
-        let contents = readFile(filePath: self.file)
-        let freqBuilder = FrequencyBuilder()
-        let frequencies = freqBuilder.getFrequency(content: contents)
-        let generator = CodeGenerator()
-        let codes = generator.generateCode(frequencies: frequencies)
-        print(frequencies)
-        print(codes)
+struct Options: ParsableArguments {
+    @Option(
+        help: "The file to read.")
+    var readFile: String
 
-        let filePath = "../testfile.bin"
-        do {
-            // replace existing content by re-creating the file
-            _ = FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil)
+    @Option(
+        help: "The file to write.")
+    var writeFile: String
+}
 
-            let fileHandle = FileHandle(forWritingAtPath: filePath)!
-            let handler = FileHandler(handle: fileHandle)
-            let serializer = CodeSerializer(ioHandler: handler, codes: codes)
-            try serializer.writeHeader(contents)
-            try serializer.writeContent(contents)
-            try fileHandle.close()
-            print("file written")
-        } catch {
-            print("Error writing file: \(error)")
+extension Huffer {
+    struct Serialize : ParsableCommand {
+        static let configuration = CommandConfiguration(
+            abstract: "Serialize a file and write the encoded content to a new file.",
+            aliases: ["se"])
+
+        @OptionGroup var options: Options
+
+        mutating func run() throws {
+            print(options.readFile)
+            let contents = readFile(filePath: options.readFile)
+            let freqBuilder = FrequencyBuilder()
+            let frequencies = freqBuilder.getFrequency(content: contents)
+            let generator = CodeGenerator()
+            let codes = generator.generateCode(frequencies: frequencies)
+            print(frequencies)
+            print(codes)
+
+            do {
+                // replace existing content by re-creating the file
+                _ = FileManager.default.createFile(atPath: options.writeFile, contents: nil, attributes: nil)
+
+                let fileHandle = FileHandle(forWritingAtPath: options.writeFile)!
+                let handler = FileHandler(handle: fileHandle)
+                let serializer = CodeSerializer(ioHandler: handler, codes: codes)
+                try serializer.writeHeader(contents)
+                try serializer.writeContent(contents)
+                try fileHandle.close()
+                print("file written")
+            } catch {
+                print("Error writing file: \(error)")
+            }
+        }
+
+        func readFile(filePath: String) -> String {
+            do {
+                let fileContents = try String(contentsOfFile: filePath, encoding: .utf8)
+                return fileContents
+            } catch {
+                print("Error reading file: \(error)")
+                Huffer.exit(withError: MyError.fileNotFound)
+            }
         }
     }
+}
 
-    func readFile(filePath: String) -> String {
-        do {
-            let fileContents = try String(contentsOfFile: filePath, encoding: .utf8)
-            return fileContents
-        } catch {
-            print("Error reading file: \(error)")
-            Huffer.exit(withError: MyError.fileNotFound)
+extension Huffer {
+    struct Deserialize : AsyncParsableCommand {
+        static let configuration = CommandConfiguration(
+            abstract: "Deserialize a file and write the content to a new file.",
+            aliases: ["de"])
+
+        @OptionGroup var options: Options
+
+        mutating func run() async throws {
+            do {
+                guard let data = FileManager.default.contents(atPath: options.readFile) else {
+                    print("file \(options.readFile) cannot be read")
+                    return
+                }
+                let sequence = contentsAsyncSequence(data)
+                let deserializer = CodeDeserializer()
+                let result = try await deserializer.deserialize(sequence)
+                _ = FileManager.default.createFile(atPath: options.writeFile, contents: result.data(using: .utf8), attributes: nil)
+                print("File \(options.writeFile) written")
+            } catch {
+                print("Error deserializing: \(error)")
+            }
+        }
+
+        // This helper function is necessary as the async FileHandle bytes or URL resourceBytes
+        // functions seem to be not available under Windows.
+        func contentsAsyncSequence(_ data: Data) -> AsyncStream<UInt8> {
+            AsyncStream { continuation in
+                Task {
+                    for byte in data {
+                        continuation.yield(byte)
+                    }
+                    continuation.finish()
+                }
+            }
         }
     }
 }
